bfcdcef1108fdf2370a4e89c69fc742ed4c6fb5a
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-07-24 13:42:10 -0400

    5. Styling Services

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+// Import Global Styles
+import GlobalStyle from "./components/GlobalStyle";
 
+// Import Pages
 import AboutUs from "./pages/AboutUs";
+
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,72 @@
 import React from 'react';
+
+// Import Images
+
 import home1 from '../img/home1.png';
+
+// Import Styled
+import styled from 'styled-components';
 
 const AboutSection = () => {
     return (
-        <div>
-            <div className="description">
+        <StyledAbout>
+            <StyledDescription>
                 <div className="title">
-                    <div className="hide">
+                    <StyledHide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2>your <span>dreams</span> come</h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2>true.</h2>
-                    </div>
+                    </StyledHide>
                 </div>
                 <p>Contact us for any photography or video ideas that ypu have. We have professionals with amazing skills.</p>
                 <button>Contact us</button>
-            </div>
-            <div className="image">
+            </StyledDescription>
+            <StyledImage>
                 <img src={home1} alt="Man holding camera" />
-            </div>
-        </div>
-    )
-}
+            </StyledImage>
+        </StyledAbout>
+    );
+};
+
+// Styled Components
+
+const StyledAbout = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: #fff;
+
+`;
+
+const StyledDescription = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+
+    h2 {
+        font-size: lighter;
+    }
+
+`;
+
+const StyledImage = styled.div`
+    flex: 1;
+    overflow: hidden;
+
+    img {
+        width: 100%;
+        height: 80vh; 
+        object-fit: cover;
+    }
+`;
+
+const StyledHide = styled.div`
+    overflow: hidden;
+`;
 
 export default AboutSection;
