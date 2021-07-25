1f9c882c6740ce3d769e0c2ce5ba52563799bf1b
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-07-24 21:58:45 -0400

    6. Styling Services

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,8 +4,9 @@ import React from 'react';
 
 import home1 from '../img/home1.png';
 
-// Import Styled
+// Import Styles
 import styled from 'styled-components';
+import { StyledAbout, StyledDescription, StyledImage, StyledHide } from '../styles';
 
 const AboutSection = () => {
     return (
@@ -31,42 +32,5 @@ const AboutSection = () => {
         </StyledAbout>
     );
 };
-
-// Styled Components
-
-const StyledAbout = styled.div`
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: #fff;
-
-`;
-
-const StyledDescription = styled.div`
-    flex: 1;
-    padding-right: 5rem;
-
-    h2 {
-        font-size: lighter;
-    }
-
-`;
-
-const StyledImage = styled.div`
-    flex: 1;
-    overflow: hidden;
-
-    img {
-        width: 100%;
-        height: 80vh; 
-        object-fit: cover;
-    }
-`;
-
-const StyledHide = styled.div`
-    overflow: hidden;
-`;
 
 export default AboutSection;
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -7,50 +7,86 @@ import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
 
 //Import Images
-
 import home2 from '../img/home2.png';
+
+// Import Styles
+import styled from 'styled-components';
+import { StyledAbout, StyledDescription, StyledImage, StyledHide } from '../styles';
 
 const ServicesSection = () => {
     return (
-        <div className="services">
-            <div className="description">
+        <StyledServices>
+            <StyledDescription>
                 <h2>High <span>quality</span> services</h2>
-                <div className="cards">
-                    <div className="card">
+                <StyledCards>
+                    <StyledCard>
                         <div className="icon">
                             <img src={clock} alt="Clock Icon" />
                             <h3>Efficient</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                    <div className="card">
+                    </StyledCard>
+                    <StyledCard>
                         <div className="icon">
                             <img src={teamwork} alt="Teamwork Icon" />
                             <h3>Teamwork</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                    <div className="card">
+                    </StyledCard>
+                    <StyledCard>
                         <div className="icon">
                             <img src={diaphragm} alt="Diaphragm Icon" />
                             <h3>Diaphragm </h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                    <div className="card">
+                    </StyledCard>
+                    <StyledCard>
                         <div className="icon">
                             <img src={money} alt="Money Icon" />
                             <h3>Affordable</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-            </div>
-            <div className="img">
+                    </StyledCard>
+                </StyledCards>
+            </StyledDescription>
+            <StyledImage>
                 <img src={home2} alt="Picture of a camera" />
-            </div>
-        </div>
+            </StyledImage>
+        </StyledServices>
     )
 }
+
+const StyledServices = styled(StyledAbout)`
+    
+    h2 {
+        padding-bottom: 5rem;
+    }
+
+    p {
+        width: 70%;
+        padding: 2rem 0rem 4rem 0rem;
+    }
+`;
+
+const StyledCards = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const StyledCard = styled.div`
+    flex-basis: 20rem;
+
+    .icon {
+        display: flex;
+        align-items: center;
+
+        h3 {
+            margin-left: 1rem;
+            background: #fff;
+            color: #000;
+            padding: 1rem;
+        }
+    }
+`;
 
 export default ServicesSection;
