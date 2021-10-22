e77529005cd40ab7f940e6a5f34dc0a428a23975
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-10-21 22:07:09 -0400

    Create custom hook to animate pages

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,91 +1,109 @@
-
 //Import Icons
-import clock from '../img/clock.svg';
-import diaphragm from '../img/diaphragm.svg';
-import money from '../img/money.svg';
-import teamwork from '../img/teamwork.svg';
+import clock from "../img/clock.svg";
+import diaphragm from "../img/diaphragm.svg";
+import money from "../img/money.svg";
+import teamwork from "../img/teamwork.svg";
 
 //Import Images
-import home2 from '../img/home2.png';
+import home2 from "../img/home2.png";
 
 // Import Styles
-import styled from 'styled-components';
-import { StyledAbout, StyledDescription, StyledImage, StyledHide } from '../styles';
+import styled from "styled-components";
+import {
+  StyledAbout,
+  StyledDescription,
+  StyledImage,
+  StyledHide,
+} from "../styles";
+
+//Import Animations
+import { scrollReveal } from "../animation";
+
+// Import Utils
+import { useScroll } from "./useScroll";
 
 const ServicesSection = () => {
-    return (
-        <StyledServices>
-            <StyledDescription>
-                <h2>High <span>quality</span> services</h2>
-                <StyledCards>
-                    <StyledCard>
-                        <div className="icon">
-                            <img src={clock} alt="Clock Icon" />
-                            <h3>Efficient</h3>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </StyledCard>
-                    <StyledCard>
-                        <div className="icon">
-                            <img src={teamwork} alt="Teamwork Icon" />
-                            <h3>Teamwork</h3>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </StyledCard>
-                    <StyledCard>
-                        <div className="icon">
-                            <img src={diaphragm} alt="Diaphragm Icon" />
-                            <h3>Diaphragm </h3>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </StyledCard>
-                    <StyledCard>
-                        <div className="icon">
-                            <img src={money} alt="Money Icon" />
-                            <h3>Affordable</h3>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </StyledCard>
-                </StyledCards>
-            </StyledDescription>
-            <StyledImage>
-                <img src={home2} alt="Picture of a camera" />
-            </StyledImage>
-        </StyledServices>
-    )
-}
+  const [element, controls] = useScroll();
+
+  return (
+    <StyledServices
+      variants={scrollReveal}
+      ref={element}
+      animate={controls}
+      initial="hidden"
+    >
+      <StyledDescription>
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <StyledCards>
+          <StyledCard>
+            <div className="icon">
+              <img src={clock} alt="Clock Icon" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </StyledCard>
+          <StyledCard>
+            <div className="icon">
+              <img src={teamwork} alt="Teamwork Icon" />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </StyledCard>
+          <StyledCard>
+            <div className="icon">
+              <img src={diaphragm} alt="Diaphragm Icon" />
+              <h3>Diaphragm </h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </StyledCard>
+          <StyledCard>
+            <div className="icon">
+              <img src={money} alt="Money Icon" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </StyledCard>
+        </StyledCards>
+      </StyledDescription>
+      <StyledImage>
+        <img src={home2} alt="Picture of a camera" />
+      </StyledImage>
+    </StyledServices>
+  );
+};
 
 const StyledServices = styled(StyledAbout)`
-    
-    h2 {
-        padding-bottom: 5rem;
-    }
+  h2 {
+    padding-bottom: 5rem;
+  }
 
-    p {
-        width: 70%;
-        padding: 2rem 0rem 4rem 0rem;
-    }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
 `;
 
 const StyledCards = styled.div`
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 const StyledCard = styled.div`
-    flex-basis: 20rem;
+  flex-basis: 20rem;
 
-    .icon {
-        display: flex;
-        align-items: center;
+  .icon {
+    display: flex;
+    align-items: center;
 
-        h3 {
-            margin-left: 1rem;
-            background: #fff;
-            color: #000;
-            padding: 1rem;
-        }
+    h3 {
+      margin-left: 1rem;
+      background: #fff;
+      color: #000;
+      padding: 1rem;
     }
+  }
 `;
 
 export default ServicesSection;
