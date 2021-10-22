6414c27846cda83eeb401f7c808304f3fe8bec09
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-10-22 06:46:32 -0400

    Continue "our work" page animation

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,110 +1,131 @@
-import React from 'react'
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 // Import Images
-import athlete from '../img/athlete-small.png';
-import theracer from '../img/theracer-small.png';
-import goodtimes from '../img/goodtimes-small.png';
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
 
 // Animations
-import { motion } from 'framer-motion';
-import { pageAnimation, fadeAnimation, photoAnimation, lineAnimation, slider, sliderContainer } from '../animation';
+import { motion } from "framer-motion";
+import {
+  pageAnimation,
+  fadeAnimation,
+  photoAnimation,
+  lineAnimation,
+  slider,
+  sliderContainer,
+} from "../animation";
+import { useScroll } from "../components/useScroll";
 
 const OurWork = () => {
-    return (
-        <StyledWork 
-        variants={pageAnimation} 
-        initial="hidden" 
-        animate="show" 
-        exit="exit"
-        style={{ background: "#fff" }}
-        >   
-            <motion.div variants={sliderContainer}>
-                <Frame1 variants={slider}></Frame1>
-                <Frame2 variants={slider}></Frame2>
-                <Frame3 variants={slider}></Frame3>
-                <Frame4 variants={slider}></Frame4>
-            </motion.div>
-            <StyledMovie>
-                <motion.h2 variants={fadeAnimation}>The Athlete</motion.h2>
-                <motion.div variants={lineAnimation} className="line"></motion.div>
-                <Link to="/work/the-athlete">
-                    <Hide>
-                        <motion.img variants={photoAnimation} src={athlete} alt="athlete" />
-                    </Hide>
-                </Link>
-            </StyledMovie>
-            <StyledMovie>
-                <h2>The Racer</h2>
-                <motion.div variants={lineAnimation} className="line"></motion.div>
-                <Link to="/work/the-racer">
-                    <img src={theracer} alt="the racer" />
-                </Link>
-            </StyledMovie>
-            <StyledMovie>
-                <h2>Good Times</h2>
-                <div className="line"></div>
-                <Link to="/work/good-times">
-                    <img src={goodtimes} alt="good times" />
-                </Link>
-            </StyledMovie>
-        </StyledWork>
-    )
-}
+  const [element, controls] = useScroll();
+  const [element2, controls2] = useScroll();
+
+  return (
+    <StyledWork
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+      style={{ background: "#fff" }}
+    >
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
+      <StyledMovie>
+        <motion.h2 variants={fadeAnimation}>The Athlete</motion.h2>
+        <motion.div variants={lineAnimation} className="line"></motion.div>
+        <Link to="/work/the-athlete">
+          <Hide>
+            <motion.img variants={photoAnimation} src={athlete} alt="athlete" />
+          </Hide>
+        </Link>
+      </StyledMovie>
+      <StyledMovie
+        ref={element}
+        variants={fadeAnimation}
+        animate={controls}
+        initial="hidden"
+      >
+        <h2>The Racer</h2>
+        <motion.div variants={lineAnimation} className="line"></motion.div>
+        <Link to="/work/the-racer">
+          <img src={theracer} alt="the racer" />
+        </Link>
+      </StyledMovie>
+      <StyledMovie
+        ref={element2}
+        variants={fadeAnimation}
+        animate={controls2}
+        initial="hidden"
+      >
+        <h2>Good Times</h2>
+        <motion.div variants={lineAnimation} className="line"></motion.div>
+        <Link to="/work/good-times">
+          <img src={goodtimes} alt="good times" />
+        </Link>
+      </StyledMovie>
+    </StyledWork>
+  );
+};
 
 const StyledWork = styled(motion.div)`
-    min-height: 100vh;
-    overflow: hidden;
-    padding: 5rem 10rem;
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
 
-    h2 {
-        padding: 1rem 0rem;
-    }
+  h2 {
+    padding: 1rem 0rem;
+  }
 `;
 
-const StyledMovie = styled.div`
-    padding-bottom: 10rem;
+const StyledMovie = styled(motion.div)`
+  padding-bottom: 10rem;
 
-    .line {
-        height: 0.5rem;
-        background: #23d997;
-        margin-bottom: 3rem;
-    }
+  .line {
+    height: 0.5rem;
+    background: #23d997;
+    margin-bottom: 3rem;
+  }
 
-    img {
-        width: 100%;
-        height: 70vh;
-        object-fit: cover;
-    }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
 `;
 
 const Hide = styled.div`
-    overflow: hidden;
+  overflow: hidden;
 `;
 
 // Frame Animation
 
 const Frame1 = styled(motion.div)`
-    position: fixed;
-    left: 0;
-    top: 10%;
-    width: 100%;
-    height: 100vh;
-    background: #fffebf;
-    z-index: 2;
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
 `;
 
 const Frame2 = styled(Frame1)`
-    background: #ff8efb;
+  background: #ff8efb;
 `;
 
 const Frame3 = styled(Frame1)`
-    background: #8ed2ff;
+  background: #8ed2ff;
 `;
 
 const Frame4 = styled(Frame1)`
-    background: #8effa0;
+  background: #8effa0;
 `;
 
 export default OurWork;
