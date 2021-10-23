d3dcae1e4f9a66cea5f082a59ad131fbf26a1cfb
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-10-22 21:50:14 -0400

    Add basic content and animation to contact page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,22 +1,76 @@
-import React from 'react';
+import styled from "styled-components";
 
 // Animations
-import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { motion } from "framer-motion";
+import { pageAnimation, titleAnimation } from "../animation";
 
 const ContactUs = () => {
-    return (
-        <motion.div 
-        variants={pageAnimation} 
-        initial="hidden" 
-        animate="show" 
-        exit="exit"
-        style={{ background: "#fff" }}
-        >
-            <h1>Contact</h1>
-        </motion.div>
-    )
-}
+  return (
+    <StyledContact
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+      style={{ background: "#fff" }}
+    >
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnimation}>Get in touch.</motion.h2>.
+        </Hide>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle />
+            <h2>Send us a message</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle />
+            <h2>Send an email</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle />
+            <h2>Social media</h2>
+          </Social>
+        </Hide>
+      </div>
+    </StyledContact>
+  );
+};
+
+const StyledContact = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: #000;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background-color: #353535;
+`;
+
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default ContactUs;
-
